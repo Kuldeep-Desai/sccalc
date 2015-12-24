32070f178738d35cf7c94c936967b34fd73cf490
Admin <admin@Admins-MacBook-Pro.local>
2015-12-24 12:02:03 +0530

    removing unnecessary folders

--- platforms/ios/build/calculator.build/Debug-iphonesimulator/calculator.build/Objects-normal/i386/main.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/admin/Workspace/calculator/platforms/ios/internal/main.m \
-  /Users/admin/Workspace/calculator/platforms/ios/internal/NativeScript/include/NativeScript.h \
-  /Users/admin/Workspace/calculator/platforms/ios/internal/NativeScript/include/TNSRuntime.h \
-  /Users/admin/Workspace/calculator/platforms/ios/internal/NativeScript/include/TNSRuntime+Inspector.h \
-  /Users/admin/Workspace/calculator/platforms/ios/internal/TNSDebugging.h
